5c89dabaaa13c92aa839c4b479dfd4319c906032
Wojciech Pastuszka <wojciech.pastuszka@gazeta.pl>
2017-10-01 20:27:04 +0200

    add more functionalities, bckg images

--- js/script.js
+++ js/script.js
@@ -3,11 +3,27 @@ $(document).ready(function () {
 //    ************************
 //        MOBILE
 //    ************************
-    
-    $('.mobilenav').click(function() {            
-        $('.menu ul').toggleClass('mobilestyle');
-        $('.menu ul').removeClass('navbar');
-    }); 
+    function hideMobileMenu() {
+      $('.slideIt').slideUp('slow', function() {
+        $('.menu ul').removeClass('mobilestyle');
+      });
+    }
+     
+    $('.mobilenav').click(function() {
+      if ($('.menu ul').hasClass('mobilestyle')) {
+        hideMobileMenu();
+      } else {
+        $('.menu ul').addClass('mobilestyle');
+        $('.slideIt').slideDown('slow');
+      }
+      $('.menu ul').removeClass('navbar');
+    });
+
+    $(window).resize(function() {
+      if($(window).width() > 800) {
+        $('.slideIt').css('display', 'block');
+      }
+    })  
 
     
 //    ********************************
@@ -15,24 +31,28 @@ $(document).ready(function () {
 //    ******************************** 
 
     
-    var sectionsLinks = $('.navigation').find('a');
-    var allSections = sectionsLinks.map(function () {
-        var section = $($(this).attr('href'));
-        if (section.length) {
-            return section;
-        }
+    var blocksLinks = $('.navigation').find('a');
+    var allBlocks = blocksLinks.map(function () {
+      var block = $($(this).attr('href'));
+      if (block.length) {
+        return block;
+      }
     });   
     
 
     $('a[href^="#"]').on('click', function (e) {        
-        e.preventDefault();
-        var mobileFix = $(window).width() > 800 ? 0 : 25;
-        var sectionId = $(this).attr('href');
-        var distanceToTop = $(sectionId).offset().top;
-        $('html, body').stop().animate({
-            scrollTop: distanceToTop - mobileFix
-        }, 1000);
-        $('.menu ul').removeClass('mobilestyle');
+      e.preventDefault();
+      var mobileFix = $(window).width() > 800 ? 0 : 25;
+      var blockId = $(this).attr('href');
+      var distanceToTop = $(blockId).offset().top;
+      $('html, body').stop().animate({
+        scrollTop: distanceToTop - mobileFix
+      }, 1000);
+      
+      if($(window).width() < 801) {
+        hideMobileMenu();  
+      }
+
     });
    
 //    ********************************
@@ -41,37 +61,34 @@ $(document).ready(function () {
     
     
     var showSideMenu = function(fromTop) {
-        if (fromTop > 20) {            
-            $('.navigation').addClass('navbar');
-        } else {
-            $('.navigation').removeClass('navbar');
-        }
+      if (fromTop > 20) {            
+        $('.navigation').addClass('navbar');
+      } else {
+        $('.navigation').removeClass('navbar');
+      }
     };
     
     $(window).scroll(function () {        
-        var fromTop = $(this).scrollTop()+10;
+      var fromTop = $(this).scrollTop()+10;
 
-        if ($(window).width() > 800) {
-            showSideMenu(fromTop);
-        }
+      if ($(window).width() > 800) {
+        showSideMenu(fromTop);
+      }
     
-        var watchedSections = allSections.map(function () {
-                if ($(this).offset().top < fromTop) {
-                    return this;
-            }
+      var watchedBlocks = allBlocks.map(function () {
+        if ($(this).offset().top < fromTop) {
+          return this;
+      }
         });
 
-        var currentSection = watchedSections[watchedSections.length - 1];
-        var id = currentSection && currentSection.length ? currentSection[0].id : "";
-        var lastSection = '';
-        
-        if (lastSection !== id) {
-            lastSection = id;
+      var currentBlock = watchedBlocks[watchedBlocks.length - 1];
+      var id = currentBlock && currentBlock.length ? currentBlock[0].id : "";
+      var lastBlock = '';
+      
+      if (lastBlock !== id) {
+        lastBlock = id;
 
-            sectionsLinks.parent().removeClass("active").end().filter("[href='#" + id + "']").parent().addClass("active");
-        }
-
+        blocksLinks.parent().removeClass("active").end().filter("[href='#" + id + "']").parent().addClass("active");
+      }
     });
-    
-
 });
